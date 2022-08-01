c4e1eb55d9638fbdb2d3f63f8676fbf5de6dc413
juzzrespecter <danisarp@gmail.com>
2022-08-01 15:43:40 +0200

    almost done

--- ft_crypt.go
+++ ft_crypt.go
@@ -9,13 +9,25 @@ import (
 
 const secret_key = "abubilla"
 
-func key_decrypt(key ) {}
+func key_decrypt(key string) ([]byte, err) {
+	dst := make([]byte, len(key))
+	c, err := aes.NewCypher(key)
+	if err != nil {
+		return []byte{}, err
+	}
+	raw_key := base32.Decode(key)
+	aes.Decrypt(dst, key)
+	return dst, nil
+}
 
-func key_encrypt(src string) string{
+func key_encrypt(src []byte) string {
 	dst := make([]byte, len(toEncrypt))
 	c, err := aes.NewCypher(secret_key)
-	
-	c.Encrypt(dst, []byte(src))
-	fmt.Println(dst)
-	return base32.Encode(dst)
+
+	if err != nil {
+		return nil, err
+	}
+	c.Encrypt(dst, src)
+	fmt.Println(dst) /* test */
+	return base32.Encode(dst), nil
 }
--- ft_otp.go
+++ ft_otp.go
@@ -5,26 +5,67 @@ import (
 	"os"
 	"log"
 	"encoding/hex"
+	"io/ioutils"
+	"time"
+	"math"
 )
 
-func generate_new_password(key_file string) {
+func totp_truncate([20]byte mac) {
+	/* Dynamic truncation */
+	offs := mac[19] & 0x0f
+	dbc1 := (mac[offs] & 0x7f) << 24
+	| (mac[offs+1]) << 16
+	| (mac[offs+2]) << 8
+	| mac[offs+3]
 
+	dbc2 := dbc1 % math.Pow(10, 6)
+	return dbc2
 }
 
-func store_new_key(user_input string) {
+func generate_new_password(key_file string) err {
+	key, err := ioutils.ReadFile(key_file)
+	if err != nil {
+		return err
+	}
+	K, err := key_decrypt(key) /* 1st param. of HMAC-SHA-1 */
+	if err != nil {
+		return err
+	}
+	T := make([]byte, 8)
+	timestamp := time.Now().Unix()
+	for i := 0; i < 4; i++ {
+		T[i + 4] = uint8(timestamp >> (24 - (i*8)))
+	} /* 2nd param. of HMAC-SHA-1 */
+	mac, err := Hmac(K, T)
+	if err != nil {
+		return err
+	}
+	totp := hmac_truncate(mac)
+	fmt.Println(totp)
+}
+
+func store_new_key(user_input string) err {
 	if len(user_input) < 64 {
-		log.Panic("error: key must not be lower than 64 bytes")
+		return errors.New("error: key must not be lower than 64 bytes")
+	}
+	plain_key, err := hex.DecodeString(user_input)
+	if err != nil {
+		return err
 	}
 	f, err := os.OpenFile("ft_otp.key", os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer f.close()
-	_, err := f.Write(key_encrypt(user_input))
+	encrypted_key, err := key_encrypt(plain_key)
 	if err != nil {
-		/* f closes when panic ? */
-		log.Panic(err)
+		return err
 	}
+	_, err := f.Write(encrypted_key)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
@@ -41,9 +82,14 @@ func main() {
 		os.Exit(1)
 	}
 	if *k != "" {
-		generate_new_password(*k)
+		err = generate_new_password(*k)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err)
+		}
 	} else {
-
-		store_new_key(*g)
+		err = store_new_key(*g)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err)
+		}
 	}
 }
